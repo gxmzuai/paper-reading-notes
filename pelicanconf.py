7a43de824c6552dd14f4ab1f5695cfa7302d4a4f
jiakai from macos <gujiakai28@gmail.com>
2024-09-13 19:40:30 +0800

    feat: initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+AUTHOR = "jiakai"
+SITENAME = "文献选读课程笔记"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "Asia/Shanghai"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (("Blog", "https://blog.gujiakai.top"),)
+
+# Social widget
+SOCIAL = (("X", "https://x.com/real_jiakai"),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
